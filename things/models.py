fb2014ca131e9fd0a147c6e9be35877e12d6477a
Zhongming Li <k21077726@kcl.ac.uk>
2022-12-06 20:04:47 +0000

    fix CharField typo

--- things/models.py
+++ things/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Thing(models.Model):
-    name = models.Charfield(max_length=30, unique=True, blank=False)
-    description = models.Charfield(max_length=120, blank=True)
+    name = models.CharField(max_length=30, unique=True, blank=False)
+    description = models.CharField(max_length=120, blank=True)
     quantity = models.IntegerField(
       default=0,
       validators=[
