7d132f2e6c4909447595b1331ac6fc9a5d460435
Zhongming Li <k21077726@kcl.ac.uk>
2022-12-06 20:02:19 +0000

    finish all three tasks first attempt

--- things/models.py
+++ things/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Thing(models.Model):
+    name = models.Charfield(max_length=30, unique=True, blank=False)
+    description = models.Charfield(max_length=120, blank=True)
+    quantity = models.IntegerField(
+      default=0,
+      validators=[
+        MaxValueValidator(100, message = "The maximum quantity is 100. "),
+        MinValueValidator(0, message = "The minimum quantity is 0. "),
+      ]
+    )
